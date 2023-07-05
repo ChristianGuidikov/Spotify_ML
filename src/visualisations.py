6a1465a7b21d8719d7aa0cfbbb16df99e3af6eb0
Christian Guidikov <christian.guidikov@gmail.com>
2023-07-05 16:30:08 +0300

    Modify README, new results as png, refactor as visualisations.py

--- src/visualisations.py
+++ src/visualisations.py
@@ -3,12 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from pathlib import Path
+import os.path
 
 matplotlib.rcParams.update({'font.size': 8})
 plt.rcParams['figure.dpi'] = 300
 
-df = pd.read_csv('./spotify_data.csv')
-
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "../data/spotify_data.csv")
+df = pd.read_csv(open(path))
 
 """
     Graph:       1
@@ -26,16 +28,20 @@ def plot_genre():
     genre_df['Counts'] = genre_df.groupby('genre')['genre'].transform('count')
     genre_df = genre_df.drop_duplicates().sort_values(by=['Counts'], ascending=False).iloc[0:10]
 
+    # Uncomment the following to create a csv file of the dataframe in the results directory
+    # filepath = Path('./results/result1.csv')
+    # genre_df.to_csv(filepath, index=False)
+
     x = np.asarray(genre_df.iloc[:, 0])
     y = np.asarray(genre_df.iloc[:, 1])
     ax = plt.bar(x, y, label='Popularity by genre')
     plt.xlabel('popularity', size=15)  # define label for the horizontal axis
     plt.ylabel('genre', size=15)  # define label for the vertical axis
-    plt.show()
 
-    # Uncomment the following to create a csv file of the dataframe in the results directory
-    # filepath = Path('./results/result1.csv')
-    # genre_df.to_csv(filepath, index=False)
+    # Uncomment the following to create a png file of the plot
+    # plt.savefig('./results/result1.png')
+
+    plt.show()
 
 
 """
@@ -47,16 +53,20 @@ def plot_genre():
 def plot_tempodance():
     tempodance_df = df[['tempo', 'danceability']].iloc[0:1000000]
 
+    # Uncomment the following to create a csv file of the dataframe in the results directory
+    # filepath = Path('./results/result2.csv')
+    # tempodance_df.to_csv(filepath, index=False)
+
     x = np.asarray(tempodance_df.iloc[:, 0])
     y = np.asarray(tempodance_df.iloc[:, 1])
     ax = plt.scatter(x, y, label='Tempo in terms of dance-ability', s=1)
     plt.xlabel('tempo', size=15)
     plt.ylabel('dance-ability', size=15)
-    plt.show()
 
-    # Uncomment the following to create a csv file of the dataframe in the results directory
-    # filepath = Path('./results/result2.csv')
-    # tempodance_df.to_csv(filepath, index=False)
+    # Uncomment the following to create a png file of the plot
+    # plt.savefig('./results/result2.png')
+
+    plt.show()
 
 
 """
@@ -74,17 +84,23 @@ def plot_genrelen():
     genrelen_df = genrelen_df[['genre', 'Mean']]\
         .drop_duplicates()\
         .sort_values(by=['Mean'], ascending=False)\
-        .iloc[0:10]
-
-    x = np.asarray(genrelen_df.iloc[:, 0])
-    y = np.asarray(genrelen_df.iloc[:, 1])
-    matplotlib.rcParams.update({'font.size': 5})
-    ax = plt.bar(x, y, label='Mean song length per genre')
-    plt.xlabel('genre', size=15)  # define label for the horizontal axis
-    plt.ylabel('mean', size=15)  # define label for the vertical axis
-    plt.show()
+        .iloc[0:30]
 
     # Uncomment the following to create a csv file of the dataframe in the results directory
     # filepath = Path('./results/result3.csv')
     # genrelen_df.to_csv(filepath, index=False)
 
+    x = np.asarray(genrelen_df.iloc[:, 0])
+    y = np.asarray(genrelen_df.iloc[:, 1])
+    matplotlib.rcParams.update({'font.size': 5})
+    ax = plt.barh(x, y, label='Mean song length per genre')
+    plt.gca().invert_yaxis()
+    plt.xlabel('genre', size=15)  # define label for the horizontal axis
+    plt.ylabel('mean', size=15)  # define label for the vertical axis
+
+    # Uncomment the following to create a png file of the plot
+    # plt.savefig('./results/result3.png')
+
+    plt.show()
+
+plot_tempodance()
