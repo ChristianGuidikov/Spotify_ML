053f791801dca814e8d9ba21026763b810c338be
Christian Guidikov <christian.guidikov@gmail.com>
2023-07-06 23:29:19 +0300

    Modify README, refactor results in two folders, add ml.py file

---
+++ src/ml.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import os.path
+import matplotlib.pyplot as plt
+import sklearn
+
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "../data/spotify_data.csv")
+df = pd.read_csv(open(path))
+
+
+def plot_popgenres():
+    """
+        Application 1:  Which genres are going to be the most popular?
+        Dataframe:      popgenres_df
+        Model:          []
+        Description:    Using model [] we were able to predict the trend of genre popularity
+    """
+
+    popgenres_df = df[['popularity', 'year', 'genre', 'danceability', 'acousticness']]\
+        .query("((acousticness >= 0.5 and genre=='acoustic') or genre!='acoustic') and popularity>50")
+
+    popgenres_df.drop(['danceability', 'acousticness'], axis=1, inplace=True)
+    popgenres_df.sort_values(by=['year', 'genre'], inplace=True)
+
+    popgenres_df['mean'] = popgenres_df.groupby(['year', 'genre'])['popularity'].transform('mean')
+    popgenres_df.drop('popularity', axis=1, inplace=True)
+    popgenres_df.drop_duplicates(inplace=True)
+    popgenres_df.fillna(0, inplace=True)
+    popgenres_df = popgenres_df[popgenres_df['year'] != 2023]
+
+    # We can choose to represent all genres, but the plot will look crowded
+    # genres = np.asarray(popgenres_df['genre'].drop_duplicates())
+
+    # We choose the genres most popular based on the visualisations in part 1
+    genres = ['hip-hop', 'dance', 'pop', 'alt-rock', 'country', 'indie-pop', 'k-pop', 'french']
+
+    for genre in genres:
+        x = np.asarray(popgenres_df.loc[popgenres_df['genre'] == genre]['year'])
+        y = np.asarray(popgenres_df.loc[popgenres_df['genre'] == genre]['mean'])
+        plt.plot(x, y, label=f'{genre}')
+
+    plt.legend()
+
+    # Uncomment the following to create a png file of the plot
+    plt.savefig('../results/ml/vis1.png')
+
+    plt.show()
+
+
+def plot_valence():
+    """
+        Application 2:  Are songs going to be more positive? (valence)
+        Dataframe:      valence_df
+        Model:          []
+        Description:    Using model [] we were able to predict the trend of song positivity
+    """
+
+    pass
+
--- src/visualisations.py
+++ src/visualisations.py
@@ -12,14 +12,16 @@ my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "../data/spotify_data.csv")
 df = pd.read_csv(open(path))
 
-"""
-    Graph:       1
-    Dataframe:   genre_df
-    Type:        Bar
-    Description: plots the popularity of each genre of music as a result 
-                 of the parameters given in dataframe popularity_df
-"""
+
 def plot_genre():
+    """
+        Graph:       1
+        Dataframe:   genre_df
+        Type:        Bar
+        Description: plots the popularity of each genre of music as a result of the parameters given in dataframe
+                     popularity_df
+    """
+
     popularity_df = df[['artist_name', 'track_name', 'popularity', 'genre', 'acousticness']].query(
         "((acousticness >= 0.5 and genre=='acoustic') or genre!='acoustic') and popularity > 50").sort_values(
         by=['popularity'], ascending=False)
@@ -29,32 +31,33 @@ def plot_genre():
     genre_df = genre_df.drop_duplicates().sort_values(by=['Counts'], ascending=False).iloc[0:10]
 
     # Uncomment the following to create a csv file of the dataframe in the results directory
-    # filepath = Path('./results/result1.csv')
+    # filepath = Path('../results/visualisations/result1.csv')
     # genre_df.to_csv(filepath, index=False)
 
     x = np.asarray(genre_df.iloc[:, 0])
     y = np.asarray(genre_df.iloc[:, 1])
     ax = plt.bar(x, y, label='Popularity by genre')
-    plt.xlabel('popularity', size=15)  # define label for the horizontal axis
-    plt.ylabel('genre', size=15)  # define label for the vertical axis
+    plt.xlabel('genre', size=15)  # define label for the horizontal axis
+    plt.ylabel('popularity', size=15)  # define label for the vertical axis
 
     # Uncomment the following to create a png file of the plot
-    # plt.savefig('./results/result1.png')
+    # plt.savefig('../results/visualisations/result1.png')
 
     plt.show()
 
 
-"""
-    Graph:       2
-    Dataframe:   tempodance_df
-    Type:        Scatter
-    Description: plots the correlation between tempo and dance-ability of songs
-"""
 def plot_tempodance():
+    """
+        Graph:       2
+        Dataframe:   tempodance_df
+        Type:        Scatter
+        Description: plots the correlation between tempo and dance-ability of songs
+    """
+
     tempodance_df = df[['tempo', 'danceability']].iloc[0:1000000]
 
     # Uncomment the following to create a csv file of the dataframe in the results directory
-    # filepath = Path('./results/result2.csv')
+    # filepath = Path('../results/visualisations/result2.csv')
     # tempodance_df.to_csv(filepath, index=False)
 
     x = np.asarray(tempodance_df.iloc[:, 0])
@@ -64,18 +67,19 @@ def plot_tempodance():
     plt.ylabel('dance-ability', size=15)
 
     # Uncomment the following to create a png file of the plot
-    # plt.savefig('./results/result2.png')
+    # plt.savefig('../results/visualisations/result2.png')
 
     plt.show()
 
 
-"""
-    Graph:       3
-    Dataframe:   genrelen_df
-    Type:        bar
-    Description: plots the mean length of songs in genres (with acoustic having at least 50% confidence)
-"""
 def plot_genrelen():
+    """
+        Graph:       3
+        Dataframe:   genrelen_df
+        Type:        bar
+        Description: plots the mean length of songs in genres (with acoustic having at least 50% confidence)
+    """
+
     filter_df = df[['genre', 'acousticness', 'duration_ms']]\
         .query("(acousticness >= 0.5 and genre=='acoustic') or genre!='acoustic'")
 
@@ -87,7 +91,7 @@ def plot_genrelen():
         .iloc[0:30]
 
     # Uncomment the following to create a csv file of the dataframe in the results directory
-    # filepath = Path('./results/result3.csv')
+    # filepath = Path('../results/visualisations/result3.csv')
     # genrelen_df.to_csv(filepath, index=False)
 
     x = np.asarray(genrelen_df.iloc[:, 0])
@@ -99,8 +103,6 @@ def plot_genrelen():
     plt.ylabel('mean', size=15)  # define label for the vertical axis
 
     # Uncomment the following to create a png file of the plot
-    # plt.savefig('./results/result3.png')
+    # plt.savefig('../results/visualisations/result3.png')
 
     plt.show()
-
-plot_tempodance()
