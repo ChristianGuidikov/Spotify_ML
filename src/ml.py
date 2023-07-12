39ee07309e077664022fe220bf3dfa68b41c539c
Christian Guidikov <christian.guidikov@gmail.com>
2023-07-12 14:20:14 +0300

    Prediction done for 1) in ml.py, add pred1.png

--- src/ml.py
+++ src/ml.py
@@ -5,7 +5,11 @@ import numpy as np
 import pandas as pd
 import os.path
 import matplotlib.pyplot as plt
-import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+
 
 my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "../data/spotify_data.csv")
@@ -14,10 +18,11 @@ df = pd.read_csv(open(path))
 
 def plot_popgenres():
     """
-        Application 1:  Which genres are going to be the most popular?
-        Dataframe:      popgenres_df
-        Model:          []
-        Description:    Using model [] we were able to predict the trend of genre popularity
+        Visualisation 1:  Most popular genres
+        Dataframe:        popgenres_df
+        Description:      Plot of the most popular genres based on mean popularity over the years. We acknowledge here
+                          that using a line plot instead of a scatter plot may be erroneous, however it is easier to
+                          visualise.
     """
 
     popgenres_df = df[['popularity', 'year', 'genre', 'danceability', 'acousticness']]\
@@ -35,13 +40,13 @@ def plot_popgenres():
     # We can choose to represent all genres, but the plot will look crowded
     # genres = np.asarray(popgenres_df['genre'].drop_duplicates())
 
-    # We choose the genres most popular based on the visualisations in part 1
-    genres = ['hip-hop', 'dance', 'pop', 'alt-rock', 'country', 'indie-pop', 'k-pop', 'french']
+    # We choose the most popular genres based on the visualisations in part 1
+    genres = ['pop', 'hip-hop', 'dance', 'alt-rock', 'country', 'indie-pop', 'k-pop', 'french']
 
     for genre in genres:
-        x = np.asarray(popgenres_df.loc[popgenres_df['genre'] == genre]['year'])
+        X = np.asarray(popgenres_df.loc[popgenres_df['genre'] == genre]['year'])
         y = np.asarray(popgenres_df.loc[popgenres_df['genre'] == genre]['mean'])
-        plt.plot(x, y, label=f'{genre}')
+        plt.plot(X, y, label=f'{genre}')
 
     plt.legend()
 
@@ -49,6 +54,54 @@ def plot_popgenres():
     # plt.savefig('../results/ml/vis1.png')
 
     plt.show()
+
+
+def plot_popgenres_pred():
+    """
+        Application 1:  Which genres are going to be the most popular?
+        Dataframe:      popgenres_df
+        Model:          Polynomial Regression
+        Description:    Using Polynomial Regression we were able to predict the trend of genre popularity amongst the
+                        3 most popular genres
+    """
+
+    popgenres_df = df[['popularity', 'year', 'genre', 'danceability', 'acousticness']] \
+        .query("((acousticness >= 0.5 and genre=='acoustic') or genre!='acoustic') and popularity>50")
+
+    popgenres_df.drop(['danceability', 'acousticness'], axis=1, inplace=True)
+    popgenres_df.sort_values(by=['year', 'genre'], inplace=True)
+
+    popgenres_df['mean'] = popgenres_df.groupby(['year', 'genre'])['popularity'].transform('mean')
+    popgenres_df.drop('popularity', axis=1, inplace=True)
+    popgenres_df.drop_duplicates(inplace=True)
+    popgenres_df.fillna(0, inplace=True)
+    popgenres_df = popgenres_df[popgenres_df['year'] != 2023]
+
+    # We choose the 3 most popular genres based on the visualisations in part 1
+    genres = ['pop', 'hip-hop', 'dance', 'k-pop']
+
+    for genre in genres:
+        X = np.asarray(popgenres_df.loc[popgenres_df['genre'] == genre]['year'])
+        y = np.asarray(popgenres_df.loc[popgenres_df['genre'] == genre]['mean'])
+
+        poly = PolynomialFeatures(degree=2, include_bias=False)
+        poly_features = poly.fit_transform(X.reshape(-1, 1))
+        poly_reg_model = LinearRegression()
+        poly_reg_model.fit(poly_features, y)
+        y_predicted = poly_reg_model.predict(poly_features)
+
+        plt.scatter(X, y, label=f"{genre}", s=2)
+        plt.plot(X, y_predicted, label=f"{genre} pred")
+
+    plt.legend()
+
+    # Uncomment the following to create a png file of the plot
+    # plt.savefig('../results/ml/pred1.png')
+
+    plt.show()
+
+
+plot_popgenres_pred()
 
 
 def plot_valence():
@@ -146,4 +199,3 @@ def plot_tempo():
     plt.show()
 
 
-plot_tempo()
